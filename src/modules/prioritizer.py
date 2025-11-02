48e63308a75963c713c843ce9fe848ea226dbdc8
NeyirethSoriano <neyirethdayanasorianoruiz@gmail.com>
2025-11-02 10:10:49 -0500

    Update prioritizer.py
    Realice limpieza y organisacion  del código

--- src/modules/prioritizer.py
+++ src/modules/prioritizer.py
@@ -1,16 +1,13 @@
 def calcular_puntaje(impacto, esfuerzo, costo):
-
     try:
         return round(impacto / (esfuerzo + costo), 3)
     except ZeroDivisionError:
         return 0.0
     except Exception:
         return 0.0
-
 def priorizar(iniciativas):
     
     iniciativas_validas = []
-
     for ini in iniciativas:
         try:
             impacto = float(ini["impacto"])
@@ -18,7 +15,6 @@ def priorizar(iniciativas):
             costo = float(ini["costo"])
         except (KeyError, ValueError, TypeError):
             continue
-
         if impacto <= 0 or esfuerzo <= 0 or costo <= 0:
             continue
 
@@ -29,12 +25,8 @@ def priorizar(iniciativas):
     return iniciativas_validas
 
 def desempatar(iniciativas):
-    
     iniciativas.sort(key=lambda x: (x["puntaje"], x["impacto"]), reverse=True)
     return iniciativas
-
-
-
 def priorizar(iniciativas):
     iniciativas_validas = []
 
@@ -45,12 +37,10 @@ def priorizar(iniciativas):
             costo = float(ini["costo"])
         except (KeyError, ValueError, TypeError):
             continue
-
         if impacto <= 0 or esfuerzo <= 0 or costo <= 0:
             continue
-
         ini["puntaje"] = calcular_puntaje(impacto, esfuerzo, costo)
         iniciativas_validas.append(ini)
-
+        
     iniciativas_validas.sort(key=lambda x: x["puntaje"], reverse=True)
     return desempatar(iniciativas_validas)
